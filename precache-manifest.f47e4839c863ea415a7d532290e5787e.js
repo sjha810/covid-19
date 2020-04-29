dfcc06c21d19a5d601339778ed88129a261a154c
Suman Jha <sumanjha810@gmail.com>
2020-04-29 16:55:49 +0530

    Updates

---
+++ precache-manifest.f47e4839c863ea415a7d532290e5787e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a800ef7954c9c4822783d7c1214429b2",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "8a4939f7d5acc6cc43e8",
+    "url": "/covid-19/static/css/main.5c873f2e.chunk.css"
+  },
+  {
+    "revision": "5338a47f72471b6d14e0",
+    "url": "/covid-19/static/js/2.b43425bb.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19/static/js/2.b43425bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a4939f7d5acc6cc43e8",
+    "url": "/covid-19/static/js/main.e70c5bce.chunk.js"
+  },
+  {
+    "revision": "5947671d1c53a9002efe",
+    "url": "/covid-19/static/js/runtime-main.db8c32d2.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19/static/media/covid-19.d7265326.png"
+  }
+]);
